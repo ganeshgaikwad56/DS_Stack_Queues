51479401d408a549e68a81b567c47578bb9058db
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-11 18:25:38 +0530

    Ganesh adding Queue program file

--- Program.cs
+++ Program.cs
@@ -6,53 +6,90 @@ namespace DataStructureGenerics
     {
         static void Main(string[] args)
         {
-            Stack<int> stack = new Stack<int>();
+            //Stack<int> stack = new Stack<int>();
 
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
+            //stack.Push(70);
+            //stack.Push(30);
+            //stack.Push(56);
 
-            foreach (int i in stack)
+            //foreach (int i in stack)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("*********After pop********");
+            ////poping
+            //stack.Pop();
+            //foreach (int i in stack)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            ////peek operation
+            //Console.WriteLine("****After Peek*****");
+            //stack.Peek();
+            //foreach (int i in stack)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            ////poping
+
+            //Console.WriteLine("****After Peek*****");
+            //stack.Pop();
+            //foreach (int i in stack)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //stack.Pop();
+            //foreach(int i in stack)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            ////Count
+
+            //stack.Peek();
+            //foreach(int i in stack)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            //Enqueue adding int
+            Queue<int> vs = new Queue<int>();
+            vs.Enqueue(56);
+            vs.Enqueue(30);
+            vs.Enqueue(70);
+
+            foreach (int i in vs)
             {
                 Console.WriteLine(i);
             }
-            Console.WriteLine("*********After pop********");
-            //poping
-            stack.Pop();
-            foreach (int i in stack)
-            {
-                Console.WriteLine(i);
-            }
-            //peek operation
-            Console.WriteLine("****After Peek*****");
-            stack.Peek();
-            foreach (int i in stack)
-            {
-                Console.WriteLine(i);
-            }
-            //poping
+            Console.WriteLine("******after Dequeue*****");
 
-            Console.WriteLine("****After Peek*****");
-            stack.Pop();
-            foreach (int i in stack)
+            vs.Dequeue();
+
+            foreach (int i in vs)
             {
                 Console.WriteLine(i);
             }
 
-            stack.Pop();
-            foreach(int i in stack)
+            Console.WriteLine("******after Dequeue*****");
+
+            vs.Dequeue();
+
+            foreach (int i in vs)
             {
                 Console.WriteLine(i);
             }
+            Console.WriteLine("******after Dequeue*****");
 
-            //Count
+            vs.Dequeue();
 
-            stack.Peek();
-            foreach(int i in stack)
+            foreach (int i in vs)
             {
                 Console.WriteLine(i);
             }
-
         }
     }
 }
